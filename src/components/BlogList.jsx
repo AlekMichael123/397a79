f2cf1d14f34aa7f323e44c0d45cedf7c300cd079
AlekMichael123 <77558217+AlekMichael123@users.noreply.github.com>
2022-08-07 20:34:25 -0700

    Revert "Revert "Implementations and Answers Completed""
    This reverts commit 3c18cc6109b51eac3fdfd455095963ce2b7639ab.

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,22 +1,56 @@
+/**
+ * @author Alek Michael
+ */
 import BlogPost from "./BlogPost";
 import Pagination from "./Pagination";
 import React from "react";
 import blogs from "../data/blogs.json";
+import { useState } from "react";
+import { useEffect } from "react";
 
 const PAGE_SIZES = [15, 25, 50, 100];
 
 function BlogList() {
-  const currentPaginationData = blogs.posts.slice(0, 15);
+  /**
+   * The first thing I changed was making these into states.
+   * This allows easier writing for me in the child components.
+   */
+  // Current Page Index
+  const [currentPage, setCurrentPage] = useState(1);
+  // Maximum items per page
+  const [currentRowsPerPage, setCurrentRowsPerPage] = useState(15);
+  // The actual user information to be displayed, calculated in useEffect below
+  const [currentPaginationData, setCurrentPaginationData] = useState([]);
+  // Anytime the page or page size change, we'll have to "re-fetch" the user data
+  useEffect(() => {
+    // if in bounds, get the full slice
+    if (currentPage * currentRowsPerPage <= blogs.posts.length)
+      setCurrentPaginationData(
+        blogs.posts.slice(
+          (currentPage - 1) * currentRowsPerPage,
+          currentPage * currentRowsPerPage
+        )
+      );
+    // otherwise, just get the rest
+    else
+      setCurrentPaginationData(
+        blogs.posts.slice(
+          (currentPage - 1) * currentRowsPerPage,
+          blogs.posts.length
+        )
+      );
+  }, [currentPage, currentRowsPerPage]);
 
-  const updateRowsPerPage = () => {};
-  const updatePage = () => {};
+  // functions to be passed down to Pagination component, changes the state here
+  const updateRowsPerPage = (rows) => setCurrentRowsPerPage(rows);
+  const updatePage = (page) => setCurrentPage(page);
 
   return (
     <div>
       <Pagination
-        currentPage={1}
+        currentPage={currentPage}
         totalCount={blogs.posts.length}
-        pageSize={15}
+        pageSize={currentRowsPerPage}
         pageSizeOptions={PAGE_SIZES}
         onPageChange={updatePage}
         onPageSizeOptionChange={updateRowsPerPage}
